3e2b340a49f5d571b7db8588f1a2874461706302
Orhan Can Cetin <orhancan_cetin@hotmail.com>
2022-10-21 17:17:55 +0300

    Mesaj atmak için gerekli kodlar oluşturuldu.

---
+++ WhatsappMesajGönderme.py
@@ -0,0 +1,7 @@
+import pywhatkit as msg
+
+msg.sendwhatmsg("mesaj atmak istediğiniz kişiye ait tel no.","İstediğiniz mesaj",Gönderilecek saat,Gönderilecek dakika)
+
+# Merhabalar Python yoluyla whatsapptan istediğiniz kişiye istediğiniz mesajı atabilirsiniz.
+# Bu kodun çalışması için Whatsapp Web'e giriş yapmanız gerekiyor.
+# Giriş yaptıktan sonra biraz beklediğinizde mesaj otomatik olarak atılıyor.
